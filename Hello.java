b5a5c79d21b191bfedb4501d7e8eea9c1d098b1e
mohanmomo <mohanchin789@gmail.com>
2018-05-29 12:23:29 +0530

    k

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+public static void main(String s[])
+{
+System.out.println("Welcome !!");
+ byte one=100;
+byte two=20;
+int three = one + two;
+System.out.println(three);
+}
+}
