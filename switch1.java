08343e3d50b34a605705464398b0d298cc05dfee
mohanmomo <mohanchin789@gmail.com>
2018-05-30 11:23:18 +0530

    h

---
+++ switch1.java
@@ -0,0 +1,15 @@
+public class switch1
+{
+public static void main(String args[])
+{
+int choice=1;
+System.out.println(choice);
+switch(choice)
+{
+case 1:
+	System.out.println("a");
+break;
+}
+
+}
+}
