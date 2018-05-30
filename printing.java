cdc908c61eac0b7a5fdee06953c3af183d4b78a7
mohanmomo <mohanchin789@gmail.com>
2018-05-30 12:33:11 +0530

    yaa

---
+++ printing.java
@@ -0,0 +1,13 @@
+public class printing
+{
+public static void main( String args[])
+{
+for ( int i = 1; i <= 10; i++ )
+{
+for ( int j = 1; j <= 5; j++ )
+System.out.print( '@' );
+
+ System.out.println();
+ } 
+ } 
+ } 
