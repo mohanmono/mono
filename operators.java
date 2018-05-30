9328deff64cf1391ba3abdd5443461b2f0562b49
mohanmomo <mohanchin789@gmail.com>
2018-05-30 10:08:02 +0530

    hi

---
+++ operators.java
@@ -0,0 +1,20 @@
+public class operators
+{
+public static void main(String args[]){
+int num=0;
+num++;
+num--;
+int negnum=20;
+boolean isTrue=false;
+System.out.println(!isTrue);
+System.out.println(num>20);
+System.out.println(num<20);
+System.out.println(num>=20);
+System.out.println(num<=20);
+System.out.println(num==20);
+System.out.println(num!=20);
+System.out.println(5&2);
+System.out.println(5^2);
+System.out.println(5|2);
+}
+}
