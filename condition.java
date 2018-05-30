f10bf9a93cef37492254c81825f8b7e3d5558c8d
mohanmomo <mohanchin789@gmail.com>
2018-05-30 10:29:18 +0530

    oh

---
+++ condition.java
@@ -0,0 +1,15 @@
+public class condition
+{
+public static void main(String args[])
+{
+ int i=0;
+while(i<=50)
+{
+if(i%2==0)
+System.out.println("Evev");
+else
+System.out.println("Odd");
+i++;
+}
+}
+}
